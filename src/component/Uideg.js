1ee25847443dfd3607caa443ea14a848e3179658
Somali <63553140+Somali19@users.noreply.github.com>
2021-06-09 08:05:43 +0530

    Update Uideg.js

--- src/component/Uideg.js
+++ src/component/Uideg.js
@@ -52,7 +52,7 @@ const Uideg = () => {
     Photography
   </Badge> </NavLink>
       </Col>
-      <Col>
+      <Col sm>
       <CardDeck className="cards">
   <Card className="cards cd">
     <Card.Img variant="top" className="h" src={uides1} />
