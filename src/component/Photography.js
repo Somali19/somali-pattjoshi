e2bba6217315dcbe70ea16b9b03270732f943e69
Somali <Somali19@users.noreply.github.com>
2021-06-09 22:14:28 +0530

    modified:   src/component/Photography.js 	modified:   src/component/Portfolio.css 	src/component/IMG_20210421_172514_479.jpg 	src/component/p1.jpeg 	src/component/p2.jpeg 	src/component/p3.jpeg 	src/component/p4.jpeg 	src/component/p5.jpeg 	src/component/p6.jpeg 	src/component/p7.jpg 	src/component/p8.jpg

--- src/component/Photography.js
+++ src/component/Photography.js
@@ -6,7 +6,14 @@ import { Badge } from "react-bootstrap";
 import { CardDeck} from "react-bootstrap";
 import { Card} from "react-bootstrap";
 import './Portfolio.css';
-import ext from './ext.png';
+import p1 from './p1.jpeg';
+import p2 from './p2.jpeg';
+import p3 from './p3.jpeg';
+import p4 from './p4.jpeg';
+import p5 from './p5.jpeg';
+import p6 from './p6.jpeg';
+import p7 from './p7.jpg';
+import p8 from './p8.jpg';
 
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import {NavLink} from 'react-router-dom';
@@ -51,11 +58,11 @@ const Photography = () => {
 
       <CardDeck className="cards">
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p1} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon<br /> 
+       <br /> 
       <hr/>
       <small className="text-muted"> <br /></small>
     
@@ -64,11 +71,11 @@ const Photography = () => {
   
   </Card> 
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p2} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon<br /> 
+       <br /> 
       <hr/>
       <small className="text-muted"> <br /></small>
     
@@ -79,11 +86,11 @@ const Photography = () => {
   </CardDeck>
    <CardDeck className="cards">
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p3} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon<br /> 
+       <br /> 
       <hr/>
       <small className="text-muted"> <br /></small>
     
@@ -92,11 +99,11 @@ const Photography = () => {
    
   </Card>
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p4} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon<br /> 
+       <br /> 
       <hr/>
       <small className="text-muted"> <br /></small>
     
@@ -107,11 +114,11 @@ const Photography = () => {
   </CardDeck>
    <CardDeck className="cards">
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p5} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon <br /> 
+        <br /> 
       <hr/>
       <small className="text-muted"> <br /></small>
     
@@ -120,11 +127,11 @@ const Photography = () => {
     
   </Card>
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p6} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon <br /> 
+        <br /> 
       <hr/>
      <small className="text-muted"> <br /></small>
     
@@ -135,11 +142,11 @@ const Photography = () => {
   </CardDeck>
    <CardDeck className="cards">
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p7} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon <br /> 
+        <br /> 
       <hr/>
       <small className="text-muted"> <br /></small>
     
@@ -148,11 +155,11 @@ const Photography = () => {
     
   </Card>
   <Card className="cards cd">
-    <Card.Img variant="top" className="h" src={ext} />
+    <Card.Img variant="top" className="h" src={p8} />
     <Card.Body>
-      <Card.Title>Coming soon</Card.Title>
+      <Card.Title> </Card.Title>
       <Card.Text>
-      Coming soon <br /> 
+       <br /> 
       <hr/>
      <small className="text-muted"> <br /></small>
     
