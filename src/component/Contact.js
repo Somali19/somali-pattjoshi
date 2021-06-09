4d3d75fe309e4532d00b7ef7a22bedd4687f0655
Somali <63553140+Somali19@users.noreply.github.com>
2021-06-09 09:08:42 +0530

    Update Contact.js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -48,6 +48,8 @@ const Contact = () => {
            
             <br />
             <br />
+           <div className="loc cont3" > Location  </div>  
+         
             <div className="mapp" style= {{width: "100%"}}><iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=junagarh%20,%20kalahandi+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
                 </Col>
             
