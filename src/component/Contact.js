0a6fdc12fd65216e950cf568169102e4a043e7ae
Somali <Somali19@users.noreply.github.com>
2021-07-20 17:38:01 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/component/Contact.js
    	modified:   src/component/Portfolio.css

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -5,6 +5,8 @@ import { Col } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
+
+import pic from './somali-pics.png';
 import './Contact.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -14,90 +16,55 @@ const Contact = () => {
   })
   AOS.refresh();
   return (
-      <div>
+<div>
          <h2 className="wk" data-aos="fade-in">  CONTACT</h2>
         <hr className="liinee" data-aos="fade-in" />
         <Container data-aos="zoom-in-up">
-          <Row >
-            <Col className="cnt">
-            
+        <br />
             <br />
-            <br />
-            <br />
-            <br />
-          
-            <br />
-            <br />
-       <Card className="cdd soc" style={{width: "24rem" }}> 
-       <Card.Body className="cont3">  Any Message? Drop your message here! <Card.Text>  <br /><br />
-            <br /> <Form className="frm">
-  <Form.Group controlId="formBasicEmail">
-  
-    <Form.Control type="email" placeholder="Enter Email" />
-  </Form.Group>
-  
-  <Form.Group controlId="formBasicMessage">
-  <Form.Control type="text" placeholder="Message" />
-  </Form.Group>
-  <br />
-  <Button className="bt" type="submit">
-    Submit
-  </Button>
- 
- 
-</Form> </Card.Text>   </Card.Body>
-</Card> 
-
-       
-            </Col>
-            <Col className="cnt">
-           
-            <br />
-            <br />
-
-            <div className="loc cont3" > Location  </div>  
-
-           
-         
-
-            <div className="mapp" style= {{width: "100%"}}><iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=junagarh%20,%20kalahandi+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
-              </Col>
-            
-          </Row>
-          <br />
-          <br />
-          <br />
-
-          <div className="soc cont3"> 
-        
-          Find me on social media -
+       <Card className="cdd soc"> 
+       <Card.Body className="cont3">   Find me on social media -
           <br />
           <hr className="liinee" />
           <br />
           <br />
-        <Col>
-       
-    <a href="https://www.linkedin.com/in/somali-pattjoshi-03748b196">  <i class="fab fa-linkedin-in"></i></a> 
+            <Row>
+            <Col>
+            <a href="https://www.linkedin.com/in/somali-pattjoshi-03748b196">  <i class="fab fa-linkedin-in"></i></a> 
+    &nbsp; &nbsp;
+      <a href= "mailto: somalipattjoshi19@gmail.com"><i class="far fa-envelope"></i> </a>
   &nbsp; &nbsp;
-  <a href= "mailto: somalipattjoshi19@gmail.com"><i class="far fa-envelope"></i> </a>
+      <a href="https://github.com/Somali19"><i class="fab fa-github"></i></a>
   &nbsp; &nbsp;
-  <a href="https://github.com/Somali19"><i class="fab fa-github"></i></a>
+  <a href="https://www.behance.net/somusomali"><i class="fab fa-behance"></i>  </a>
   &nbsp; &nbsp;
- <a href="https://www.behance.net/somusomali"><i class="fab fa-behance"></i>  </a>
- &nbsp; &nbsp;
   <a href="https://dribbble.com/__Somali__"><i class="fab fa-dribbble"></i> </a>
   
- </Col> 
-   
- </div>
-<br />
-<br />
-<i class="fab fa-gratipay"></i>
-<br />
-<div className="cont3">Thank you for visiting. </div>
-   
-        </Container>
-      </div>
+    </Col>
+        <Col>
+          <img src={pic} alt="pic" className="bg" data-aos="zoom-in-up" />
+    
+          </Col>
+           
+        </Row>
+     </Card.Body>
+  </Card> 
+    <div className="loc cont3" > Location -  </div>  
+
+           
+         
+
+    <div className="mapp" style= {{width: "100%"}}><iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=junagarh%20,%20kalahandi+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+    <br />
+  <br />
+  <i class="fab fa-gratipay"></i>
+    <br />
+  <div className="cont3">Thank you for visiting. 
+  <br /> <br />
+    </div>
+          </Container>
+       
+</div>
   )
 }
 
